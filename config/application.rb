879a71727c422d0eeec28586aa4a34d650f22770
Jan Varwig <jan@varwig.org>
2012-04-29 14:33:41 +0200

    rails 3.0.8

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require "action_controller/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  # Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -56,10 +56,5 @@ module TestMobile
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
   end
 end
